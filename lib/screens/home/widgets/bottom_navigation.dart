7770d7b9360b9bd1691495486e8210fbc4048426
GonFreecs10 <yazincj@gmail.com>
2023-05-10 00:30:04 +0530

    first commit

---
+++ lib/screens/home/widgets/bottom_navigation.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:money_manager_flutter/screens/home/screen_home.dart';
+
+class MoneyManagerBottomNavigation extends StatelessWidget {
+  const MoneyManagerBottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: ScreenHome.selectedIndexNotifier,
+      builder: (BuildContext ctx, int updatedIndex, Widget?_) {
+        return BottomNavigationBar(selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.grey[100],
+        currentIndex: updatedIndex,
+        onTap: (newIndex) {
+          ScreenHome.selectedIndexNotifier.value = newIndex;
+          
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Transactions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: 'categories'),
+        ],
+      );
+      },
+    );
+  }
+}
